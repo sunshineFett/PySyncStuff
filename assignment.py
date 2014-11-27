6a9362eb761a1913685e524c8206a6713cca7873
Sunny <sunnywear@tampabay.rr.com>
2014-11-26 19:10:17 -0500

    added functions

--- assignment.py
+++ assignment.py
@@ -1,13 +1,19 @@
 # My first python program
 
+def newfunction() :
+    print "this is a new function"
+    
+def addMyName(num1, num2):
+    print(num1 + num2)
+
 # Hello World in Python
 print "Hello World!"
 
 print "My first Python program!!"
 
-
-def newfunction() :
-    print "this is a new function"
-    
+num1 = "S"
+num2 = "W"
+      
+addMyName(num1, num2)    
     
     
